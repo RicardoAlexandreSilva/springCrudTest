11d8b1f342a94ba38d17d59fc21ff805e6acda4a
Ricardo Alexandre <silva1620costa@gmail.com>
2024-10-27 13:05:20 -0400

    config area de trabalho

---
+++ forms-register/src/main/java/projeto/fatec/FormsRegisterApplication.java
@@ -0,0 +1,13 @@
+package projeto.fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsRegisterApplication.class, args);
+	}
+
+}
---
+++ forms-register/src/test/java/projeto/fatec/FormsRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
